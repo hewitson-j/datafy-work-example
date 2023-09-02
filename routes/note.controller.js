940d6ca34b6936a83c58ce050dd17f00347edc62
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-09-01 19:42:53 -0600

    Added post, update to routes.

--- routes/note.controller.js
+++ routes/note.controller.js
@@ -6,51 +6,53 @@ const router = Router();
 const noteRepository = AppDataSource.getRepository(Note);
 
 // Create new record
-// router.post('/notes', async (req, res) => {
-//   const { title, body } = req.body;
-//   const note = new Note();
-//   note.title = title;
-//   note.body = body;
+router.post("/notes", async (req, res) => {
+  const { title, body } = req.body;
+  const note = new Note();
+  note.title = title;
+  note.body = body;
 
-//   try {
-//     const savedNote = await noteRepository.save(note);
-//     return res.status(201).json(savedNote);
-//   } catch (error) {
-//     return res.status(500).json({ error: 'Unable to create note' });
-//   }
-// });
+  try {
+    const savedNote = await noteRepository.save(note);
+    console.log("Record added successfully.");
+    return res.status(201).json(savedNote);
+  } catch (error) {
+    return res.status(500).json({ error: "Unable to create note" });
+  }
+});
 
 // Get All
 router.get("/notes", async (_, res) => {
   const notes = await noteRepository.find();
+  console.log("All records returned successfully.");
   return res.json(notes);
 });
 
 // Get by ID
-// router.get("/notes/:id", async (req, res) => {
-//   const { id } = req.params;
-//   const note = await noteRepository.findOne(id);
+router.get("/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  const note = await noteRepository.findOne(id);
 
-//   if (!note) {
-//     return res.status(404).json({ error: "Note not found" });
-//   }
+  if (!note) {
+    return res.status(404).json({ error: "Note not found" });
+  }
 
-//   return res.json(note);
-// });
+  return res.json(note);
+});
 
 // Update by ID
-// router.put('/notes/:id', async (req, res) => {
-//   const { id } = req.params;
-//   const { title, body } = req.body;
+router.put("/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, body } = req.body;
 
-//   try {
-//     await noteRepository.update(id, { title, body });
-//     const updatedNote = await noteRepository.findOne(id);
-//     return res.json(updatedNote);
-//   } catch (error) {
-//     return res.status(500).json({ error: 'Unable to update note' });
-//   }
-// });
+  try {
+    await noteRepository.update(id, { title, body });
+    const updatedNote = await noteRepository.findOne(id);
+    return res.json(updatedNote);
+  } catch (error) {
+    return res.status(500).json({ error: "Unable to update note" });
+  }
+});
 
 // Delete by ID
 router.delete("/notes/:id", async (req, res) => {
